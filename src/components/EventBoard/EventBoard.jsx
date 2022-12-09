e709478aa0f19ae81f6dd3d68e37d0e239b6ea0c
Maryna-Korbet <m22v86k@gmail.com>
2022-12-09 20:07:14 +0200

    added PropTypes

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,5 +1,10 @@
+import PropTypes from 'prop-types';
 import css from '../EventBoard/EventBoard.module.css';
 
 export const EventBoard = ({ events }) => {
     return <div className={css.eventBoard}>Event cards</div>;
+}
+
+EventBoard.propTypes = {
+    events: PropTypes.array.isRequired,
 }
