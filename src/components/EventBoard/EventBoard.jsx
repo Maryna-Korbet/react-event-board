ed31642c40e6e039f4b4bd913204e86e3386d026
Maryna-Korbet <m22v86k@gmail.com>
2022-12-09 21:14:58 +0200

    Update EventBoard.jsx

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -2,7 +2,11 @@ import PropTypes from 'prop-types';
 import css from '../EventBoard/EventBoard.module.css';
 
 export const EventBoard = ({ events }) => {
-    return <div className={css.eventBoard}>Event cards</div>;
+    return <div className={css.eventBoard}>{events.map(
+        event => (
+            <div key={event.name}>{event.name}</div>                
+        )
+    )}</div>;
 }
 
 EventBoard.propTypes = {
